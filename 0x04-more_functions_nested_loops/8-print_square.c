c4d3b36e5f1ba361f84b4a57fff06ac2f543dd16
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-18 14:42:57 +0300

    SquareS

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,21 +7,22 @@
  */
 void print_square(int size)
 {
-
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 int i;
 int j;
 
-if (size > 0)
-{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
-_putchar('#');
-else
 {
-_putchar('\n');
-}
-}
+_putchar('#');
 }
 _putchar('\n');
+}
+}
 }
