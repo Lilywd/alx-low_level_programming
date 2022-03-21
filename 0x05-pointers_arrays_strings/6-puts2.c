5d8089ce816abe5339913cc2c1fa17cc9b9bebf5
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-21 15:41:06 +0300

    Prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,7 @@
 
 /**
  * puts2 - function that prints every other character of a string
- * starting with the first character 
+ * starting with the first character
  * followed by a new line
  * @str: string to print characters from
  */
