f5fde74706bc07a3eb6c000f24be8aefbcc74615
Lilywd <ndungulilianwanjiku@gmail.com>
2022-04-13 13:18:29 +0300

    pointer to print name func

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - prints a name
+ * @name: string to print
+ * @f: function pointer
+ */
+
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
